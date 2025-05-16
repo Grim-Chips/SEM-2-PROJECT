1d3521df59ef759a4874435ded2340680fc7a3d1
GrimChips <Ak48umar2@gmail.com>
2025-05-16 15:29:14 +0300

    class units

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-import tkinter as tk 
-import pandas as pd
-import seaborn as sns 
-import matplotlib as plt
+class Units:
+    def __init__(self, UnitId, location, size, status, rent, sellingprice):
+        self.UnitId = UnitId
+        self.location = location
+        self.size = size
+        self.status = status 
+        self.rent = rent
+        self.sellingprice = sellingprice
 
+    def markedasRented(self):
+        self.status = "Rented"
 
-class properties:
-    def __init__(self):
-        
+    def markedasSold(self):
+        self.status = "Sold"
+    
+    def __str__(self):
+        return f"{self.UnitId.title()} | {self.location} | ${self.size} | {self.status} | ${self.rent} | ${self.sellingprice}"
+
+# to check if it works
+if __name__ == "__main__":
+    unit = Units("unit101", "Downtown", 1200, "Rented", None, 300000)
+    if unit.status == "Rented":
+        unit.markedasRented()
+    print(unit)
+
+    # unit.markedasRented()
+    # print(unit)
