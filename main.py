8f0ec4792dc1372a92f0fbb2d5df0100c5e5d319
GrimChips <Ak48umar2@gmail.com>
2025-05-16 14:25:29 +0300

    int

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib as plt
 
-root = tk.Tk()
-canvas = tk.Canvas(root, width=600, )
-root.mainloop()
+
+class properties:
+    def __init__(self):
+        
